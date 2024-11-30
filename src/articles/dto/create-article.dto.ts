6f592b3e13006498f774f186cb0b51d3b9c964ca
DRucci-88 <d.rucci.2001@gmail.com>
2024-11-30 20:29:13 +0700

    Input Validation and Transformation

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,13 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   title: string;
 
   @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   description?: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   body: string;
 
   @ApiProperty({ required: false, default: false })
