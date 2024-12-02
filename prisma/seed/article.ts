e7122a957853871d30d577f99c05310c6de945a2
DRucci-88 <d.rucci.2001@gmail.com>
2024-12-02 22:41:08 +0700

    Update migration and seed

--- prisma/seed/article.ts
+++ prisma/seed/article.ts
@@ -9,7 +9,9 @@ export async function articleSeed(prisma: PrismaClient) {
   // Create two dummy articles
   const post1 = await prisma.article.upsert({
     where: { title: 'Prisma Adds Support for MongoDB' },
-    update: {},
+    update: {
+      authorId: user1?.id ?? 1,
+    },
     create: {
       title: 'Prisma Adds Support for MongoDB',
       body: 'Support for MongoDB has been one of the most requested features since the initial release of...',
@@ -22,7 +24,9 @@ export async function articleSeed(prisma: PrismaClient) {
 
   const post2 = await prisma.article.upsert({
     where: { title: "What's new in Prisma? (Q1/22)" },
-    update: {},
+    update: {
+      authorId: user1?.id ?? 1,
+    },
     create: {
       title: "What's new in Prisma? (Q1/22)",
       body: 'Our engineers have been working hard, issuing new releases with many improvements...',
@@ -30,6 +34,18 @@ export async function articleSeed(prisma: PrismaClient) {
         'Learn about everything in the Prisma ecosystem and community from January to March 2022.',
       published: true,
       authorId: user1?.id ?? 1,
+    },
+  });
+
+  const post3 = await prisma.article.upsert({
+    where: { title: 'Prisma Client Just Became a Lot More Flexible' },
+    update: {},
+    create: {
+      title: 'Prisma Client Just Became a Lot More Flexible',
+      body: 'Prisma Client extensions provide a powerful new way to add functionality to Prisma in a type-safe manner...',
+      description:
+        'This article will explore various ways you can use Prisma Client extensions to add custom functionality to Prisma Client..',
+      published: true,
     },
   });
 
